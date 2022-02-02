fe7adcd532455de2a639ac8f5f9c63c02630f426
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-02-02 21:26:52 +0900

    chore: update nuxt configuration related files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt_content_chakra_ui_blog",
+    title: 'nuxt_content_chakra_ui_blog',
     htmlAttrs: {
-      lang: "en",
+      lang: 'en'
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -29,22 +29,22 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/chakra
-    "@chakra-ui/nuxt",
+    '@chakra-ui/nuxt',
     // https://go.nuxtjs.dev/emotion
-    "@nuxtjs/emotion",
+    '@nuxtjs/emotion',
     // https://go.nuxtjs.dev/content
-    "@nuxt/content",
+    '@nuxt/content'
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-};
+  build: {}
+}
